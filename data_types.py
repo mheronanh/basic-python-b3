4f1554b34e35c5527acb7a12ea52681d18e850ef
mheronanh <muhammadheronan@gmail.com>
2021-01-09 14:47:24 +0700

    Pertemuan 1 - Basic Python

---
+++ data_types.py
@@ -0,0 +1,21 @@
+sapa = "Hai, teman-teman"
+umur = 20
+tinggi = 174.5
+laki_laki = True
+
+#melakukan data 
+print(type(sapa))
+print(type(umur))
+print(type(tinggi))
+print(type(laki_laki))
+
+#Melakukan data casting
+int_to_float = float(umur)
+print(type(int_to_float))
+float_to_int = int(tinggi)
+print(type(float_to_int))
+int_to_str = str(umur)
+print(type(int_to_str))
+
+
+
